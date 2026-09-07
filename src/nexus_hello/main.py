3d5535bade4e85bdf65e06a1725603cd5704e81e
George Zubrienko <gzu@ecco.com>
2026-09-07 12:28:25 +0200

    Upgrade SDK and switch to uv

--- src/nexus_hello/main.py
+++ src/nexus_hello/main.py
@@ -1,6 +1,7 @@
 import asyncio
 import os
 
+from nexus_client_sdk.nexus.configurations.configuration_model import NexusConfigurationModel
 from nexus_client_sdk.nexus.core.app_core import Nexus
 from nexus_client_sdk.nexus.input.command_line import NexusDefaultArguments
 
@@ -8,7 +9,7 @@ from nexus_hello.hello_algorithm import HelloAlgorithm
 from nexus_hello.models.payload import HelloData
 
 
-def logger_tags_from_payload(payload: HelloData, _: NexusDefaultArguments) -> dict[str, str]:
+def logger_tags_from_payload(payload: HelloData, _: NexusDefaultArguments, __: NexusConfigurationModel) -> dict[str, str]:
     """
     Extracts tags from payload used for logger
     """
@@ -17,7 +18,7 @@ def logger_tags_from_payload(payload: HelloData, _: NexusDefaultArguments) -> di
     }
 
 
-def metric_tags_from_payload(payload: HelloData, run_args: NexusDefaultArguments) -> dict[str, str]:
+def metric_tags_from_payload(payload: HelloData, run_args: NexusDefaultArguments, __: NexusConfigurationModel) -> dict[str, str]:
     """
     Extracts tags from payload used for metrics
     """
@@ -28,7 +29,7 @@ def metric_tags_from_payload(payload: HelloData, run_args: NexusDefaultArguments
     }
 
 
-def enrich_logger_from_payload(_: HelloData, run_args: NexusDefaultArguments) -> dict[str, dict[str, str]]:
+def enrich_logger_from_payload(_: HelloData, run_args: NexusDefaultArguments, __: NexusConfigurationModel) -> dict[str, dict[str, str]]:
     """
     Extracts tags from payload used for logging
     """
@@ -45,12 +46,6 @@ async def main():
     nexus._run_args.sas_uri = nexus._run_args.sas_uri.replace(
         "http://localhost:9000",
         os.getenv("PROTEUS__AWS_ENDPOINT", "http://localhost:9000"),
-    )
-    nexus = (
-        nexus.use_algorithm(HelloAlgorithm)
-        .inject_payload(HelloData)
-        .with_log_enricher(tagger=logger_tags_from_payload, enricher=enrich_logger_from_payload)
-        .with_metric_tagger(tagger=metric_tags_from_payload)
     )
 
     await nexus.activate()
